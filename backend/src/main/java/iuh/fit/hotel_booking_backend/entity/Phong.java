bd44f0151fbe795b683232dc66b3571af147dc33
Tri Pham <phamthanhtri0712@gmail.com>
2025-10-23 10:31:58 +0700

    push backend

---
+++ backend/src/main/java/iuh/fit/hotel_booking_backend/entity/Phong.java
@@ -0,0 +1,25 @@
+package iuh.fit.hotel_booking_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "phong")
+public class Phong {
+    @Id
+    @Column(name = "maPhong")
+    private String maPhong;
+    @ManyToOne
+    @JoinColumn(name = "ma_loai_phong", nullable = false)
+    private LoaiPhong loaiPhong;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "trang_thai")
+    private TrangThaiPhong trangThai;
+    @Column(name = "vi_tri")
+    private String viTri;
+}
---
+++ backend/src/main/java/iuh/fit/hotel_booking_backend/entity/TaiKhoan.java
@@ -0,0 +1,27 @@
+package iuh.fit.hotel_booking_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tai_khoan")
+public class TaiKhoan {
+    @Id
+    @Column(name = "ma_tai_khoan")
+    private String maTaiKhoan;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "vai_tro")
+    private LoaiTaiKhoan vaiTro;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ma_khach_hang", unique = true)
+    private KhachHang khachHang;
+    @Column(unique = true)
+    private String email;
+    @Column(name = "mat_khau")
+    private String matKhau;
+}
