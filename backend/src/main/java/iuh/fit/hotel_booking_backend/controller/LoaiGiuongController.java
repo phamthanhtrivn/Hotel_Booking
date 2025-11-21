79bc30be2ac45f52f225e510904f03c57f7eed3c
Tri Pham <phamthanhtri0712@gmail.com>
2025-11-21 12:12:29 +0700

    update LoaiGiuongController

--- backend/src/main/java/iuh/fit/hotel_booking_backend/controller/LoaiGiuongController.java
+++ backend/src/main/java/iuh/fit/hotel_booking_backend/controller/LoaiGiuongController.java
@@ -4,10 +4,7 @@ import iuh.fit.hotel_booking_backend.dto.APIResponse;
 import iuh.fit.hotel_booking_backend.entity.LoaiGiuong;
 import iuh.fit.hotel_booking_backend.service.LoaiGiuongService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -50,6 +47,5 @@ public class LoaiGiuongController {
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteLoaiGiuong(@PathVariable String id) {
         return ResponseEntity.ok(loaiGiuongService.deleteById(id));
->>>>>>> TTri
     }
 }
